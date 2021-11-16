8668b5fc2a5cf741fd2b7d7b44c028fcef670321
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2021-11-16 13:30:29 -0300

    Darkmode Adicionado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import initConverter from "./modules/aplication-converter.js";
+import OnDarkMode from "./modules/darkmod.js";
+
+const darkMode = new OnDarkMode("[data-js='switch-mode']", "darked");
+darkMode.init();
+console.log(darkMode);
 
 initConverter();
-
