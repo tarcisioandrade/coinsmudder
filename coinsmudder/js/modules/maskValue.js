b9f1a7c40cba33b8754141d906e58f32b8aca883
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2021-11-16 14:02:10 -0300

    removi uma pasta desnecessária

--- coinsmudder/js/modules/maskValue.js
+++
@@ -1,8 +0,0 @@
-function k(i) {
-  let v = i.value.replace(/\D/g, '');
-  v = (v / 100).toFixed(2) + '';
-  v = v.replace(".", ",");
-  v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
-  v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
-  i.value = v;
-}
--- coinsmudder/js/script.js
+++
@@ -1,8 +0,0 @@
-import initConverter from "./modules/aplication-converter.js";
-import OnDarkMode from "./modules/darkmod.js";
-
-const darkMode = new OnDarkMode("[data-js='switch-mode']", "darked");
-darkMode.init();
-console.log(darkMode);
-
-initConverter();
